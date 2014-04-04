d3ec685d8cfef80759a03924ef1819f589ae1f60
PedroPumada <pgzink@gmail.com>
2014-04-03 22:36:37 -0400

    Made view_users.php paginated with alternating table row colors

--- view_users.php
+++ view_users.php
@@ -1,64 +1,114 @@
-<?php # Script 9.4 - view_users.php
-// This script retrieves all the records from the users table.
+<?php # Script 10.4 - view_users.php #4
+// This script retrieves all the records from the users table
+// This new version paginates the query results
 
 $page_title = 'View the Current Users';
 include ('includes/header.html');
+echo '<h1>Registered Users</h1>';
 
-// Page header:
-echo '<h1>Registered Users';
+require_once('../../mysqli_connect.php');
 
-require('../../mysqli_connect.php'); // Connect to the db
+// Number of records to show per page:
+$display = 10;
 
-// Make the query:
-$q = "SELECT last_name, first_name, DATE_FORMAT(registration_date, '%M %d %Y') AS dr, user_id
-FROM users ORDER BY registration_date ASC";
-$r = @mysqli_query($dbc, $q); // Run the query.
+// Determine how many pages there are.....
+if (isset($_GET['p']) && is_numeric($_GET['p'])) { // Already been determined.
 
-$num = mysqli_num_rows($r);
-if ($r) { // If the query was successful with no errors
+	$pages = $_GET['p'];
 
-	if ($num > 0) { // If there are actually records, display the records
+} else { // Need to determine.
 
-		echo "<p>There are currently $num registered users.</p>\n";
+	// Count the number of records:
+	$q = "SELECT COUNT(user_id) FROM users";
+	$r = @mysqli_query($dbc, $q);
+	$row = @mysqli_fetch_array($r, MYSQLI_NUM);
+	$records = $row[0];
 
-		// Table header
-		echo '<table align="center" cellspacing="3" cellpadding="3" width="75%">
-		<tr><td align="left"><b>Edit</b></td>
-		<td align="left"><b>Delete</b></td>
-		<td align="left"><b>Last Name</b></td>
-		<td align="left"><b>First Name</b></td>
-		<td align="left"><b>Date Registered</b></td>
-		</tr>';
+	// Calculate the number of pages...
+	if ($records > $display) { // More than 1 page.
+		$pages = ceil($records/$display);
+	} else {
+		$pages = 1;
+	} 
 
-		// Fetch and print all the records:
-		while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-			echo '<tr>
-			<td align="left"><a href="edit_user.php?id=' . $row['user_id'] . '">Edit</a></td>
-			<td align="left"><a href="delete_user.php?id=' . $row['user_id'] . '">Delete</a></td>
-			<td align="left">' . $row['last_name'] . '</td>
-			<td align="left">' . $row['first_name'] . '</td>
-			<td align="left">' . $row['dr'] . '</td>
-			</tr>';
- 		}
-		echo '</table>'; // Close the table.
+} // end of p IF.
 
-		mysqli_free_result($r); // Free up the resources.
-	} else { // If no records were returned
-
-		// Public message:
-		echo '<p class="error">There are curently no registered users.</p>';
-
-		// Debugging messaage:
-		echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
-
-	}	// End of if($r) IF.
+// Determine where in the database to start returning results...
+if (isset($_GET['s']) && is_numeric($_GET['s'])) {
+	$start = $_GET['s'];
 } else {
-	echo '<p class="error">This application was unable to retrieve the current users.</p>';
+	$start = 0;
+}
 
-	// Debug message:
-	echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
-} // End of if($r) IF
-mysqli_close($dbc); // Close the database connection
+// Define the query:
+$q = "SELECT last_name, first_name, DATE_FORMAT(registration_date, '%M %d, %Y') 
+AS dr, user_id FROM users ORDER BY registration_date ASC LIMIT $start, $display";
+$r = @mysqli_query($dbc, $q);
+
+// Table header:
+echo '<table align="center" cellspacing="0" cellpadding="5" width="75%">
+<tr>
+	<td align="left"><b>Edit</b></td>
+	<td align="left"><b>Delete</b></td>
+	<td align="left"><b>Last Name</b></td>
+	<td align="left"><b>First Name</b></td>
+	<td align="left"><b>Date Registered</b></td>
+</tr>';
+
+// Fetch and print all records...
+
+$bg = '#eeeeee'; // Set the initial background color
+
+while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+
+	$bg = ($bg == '#eeeeee' ? '#ffffff' : '#eeeeee'); // Switch the background color
+	echo '<tr bgcolor="' . $bg . '">
+		<td align="left"><a href="edit_user.php?id=' . $row['user_id'] . '">Edit</a></td>
+		<td align="left"><a href="delete_user.php?id=' . $row['user_id'] . '">Delete</a></td>
+		<td align="left">' . $row['last_name'] . '</td>
+		<td align="left">' . $row['first_name'] . '</td>
+		<td align="left">' . $row['dr'] . '</td>
+		</tr>
+		';
+
+} // End of WHILE loop.
+
+echo '</table>';
+mysqli_free_result($r);
+mysqli_close($dbc);
+
+// Make the links to other pages, if necessary.
+if ($pages > 1) {
+
+	// Add some spacing and start a paragraph:
+	echo '<br /><p>';
+
+	// Determine what page the script is on:
+
+	$current_page = ($start/$display) + 1;
+
+	// If it's not the first page, make a previous link:
+	if ($current_page != 1) {
+		echo '<a href="view_users.php?s=' . ($start-$display) . '&p=' . $pages . '">Previous</a> ';
+	}
+
+	// Make all the numbered pages:
+	for ($i = 1; $i <= $pages; $i++) {
+		if ($i != $current_page) {
+			echo '<a href="view_users.php?s=' . (($display * ($i-1))) . '&p=' . $pages . '">' . $i . '</a>';
+		} else {
+			echo $i . ' ';
+		}
+	} // end for FOR loop
+
+	// If it's not the last page, make a NEXT button:
+	if ($current_page != $pages) {
+		echo '<a href="view_users.php?s=' . ($start + $display) . '&p=' . $pages . '">Next</a>';
+	}
+
+	echo '</p>'; // Close the paragraph.
+
+} // End of links section
 
 include ('includes/footer.html');
 ?>
