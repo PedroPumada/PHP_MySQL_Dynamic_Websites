2e279caedd8f50b6ea28873ab2610e0ed229dae6
PedroPumada <pgzink@gmail.com>
2014-04-13 19:26:23 -0400

    Ch 13: Added a spam filtering function to contact form

--- email.php
+++ email.php
@@ -6,22 +6,50 @@
 </head>
 <body>
 <h1>Contact Me</h1>
-<?php # Script 11.1 - email.php
+<?php # Script 13.1 - email.php #2
+// This version now scrubs dangerous strings form the submitted input.
 
 // Check for form submission:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+/* The function takes one argument: a string.
+* The function returns a clean version of the string
+* The clean version may either be an empty string or
+* just the removal of all newline characters. 
+*/
+	function spam_scrubber($value) {
+
+		// List of very bad values:
+		$very_bad = array('to:', 'cc:', 'bcc:', 'content-type:', 'mime-version:', 'multipart-mixed:', 'content-transfer-encoding:');
+
+		// If any of the very bad strings are in
+		// the submitted value, return an empty string:
+		foreach ($very_bad as $v) {
+			if (stripos($value, $v) !== false) return '';
+		}
+
+		// Replace any newline characters with spaces:
+		$value = str_replace(array( "\r", "\n", "%0a", "%0d"), ' ', $value);
+
+		// Return the value:
+		return trim($value);
+
+	} // End of spam_scrubber() function
+
+	// Clean the form data:
+	$scrubbed = array_map('spam_scrubber', $_POST);
+
 	// Minimal form validation:
-	if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['comments']) ) {
+	if (!empty($scrubbed['name']) && !empty($scrubbed['email']) && !empty($scrubbed['comments']) ) {
 
 		// Create the body:
-		$body = "Name: {$_POST['name']}\n\nComments: {$_POST['comments']}";
+		$body = "Name: {$scrubbed['name']}\n\nComments: {$scrubbed['comments']}";
 
 		// Make it no longer than 70 characters long:
 		$body = wordwrap($body, 70);
 
 		// Send the e-mail:
-		mail('pgzink@gmail.com', 'Contact Form Submission', $body, "From: {$_POST['email']}");
+		mail('pgzink@gmail.com', 'Contact Form Submission', $body, "From: {$scrubbed['email']}");
 
 		// Print a message:
 		echo '<p><em>Thank you for contacting me. I will reply some day.</em></p>';
@@ -35,17 +63,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 } // end of main isset() IF
 
+
 // Create the HTML form:
 ?>
 <p>Please fill out this form to contact me.</p>
 <form action="email.php" method="post">
-<p>Name: <input type="text" name="name" size="30" maxlength="60" value="<?php if (isset($_POST['name'])) echo $_POST['name']; ?>" /></p>
-<p>Email Address: <input type="text" name="email" size="30" maxlength="80" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" /></p>
-<p>Comments: <textarea name="comments" rows="5" cols="30"><?php if (isset($_POST['comments'])) echo $_POST['comments']; ?></textarea></p>
+<p>Name: <input type="text" name="name" size="30" maxlength="60" value="<?php if (isset($scrubbed['name'])) echo $scrubbed['name']; ?>" /></p>
+<p>Email Address: <input type="text" name="email" size="30" maxlength="80" value="<?php if (isset($scrubbed['email'])) echo $scrubbed['email']; ?>" /></p>
+<p>Comments: <textarea name="comments" rows="5" cols="30"><?php if (isset($scrubbed['comments'])) echo $scrubbed['comments']; ?></textarea></p>
 <p><input type="submit" name="submit" value="Send!" /></p>
 </form>
 </body>
 </html>
-
-
-
